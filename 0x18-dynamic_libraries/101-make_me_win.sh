bbad331e5ccc069df4ad43995a65ed9965978cbe
Itsmery <gl.mimi2020@gmail.com>
2023-07-25 05:13:59 -0700

    task3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/master/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
