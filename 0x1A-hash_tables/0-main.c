da90b126f4884ef5b8ad4a898619b63732f98fd5
Itsmery <gl.mimi2020@gmail.com>
2023-08-10 04:01:55 -0700

    first file sdded

---
+++ 0x1A-hash_tables/0-main.c
@@ -0,0 +1,13 @@
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
+}
