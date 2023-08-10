05480d6062168395e9b9b24bb7ef8954d82821b9
Itsmery <gl.mimi2020@gmail.com>
2023-08-10 04:07:32 -0700

    let's Hash

---
+++ 0x1A-hash_tables/1-main.c
@@ -0,0 +1,17 @@
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    char *s;
+
+    s = "cisfun";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    s = "Don't forget to tweet today";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    s = "98";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    return (EXIT_SUCCESS);
+}
